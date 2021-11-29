edabe079913b5c9fa81a01eddb077926fbdd647e
SimonS98 <80315339+SimonS98@users.noreply.github.com>
2021-11-30 04:02:54 +0800

    Update arduino_secrets.h

--- Progress_record/Final_Project/IOT_Fragrance_Sixiong_Sheng/arduino_secrets.h
+++ Progress_record/Final_Project/IOT_Fragrance_Sixiong_Sheng/arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID "SimonRock"
-#define SECRET_PSW "Wuchen21"
-#define SECRET_APIKEY "4cf53cfc2c365ed7e36026acca1dc6c1"
+#define SECRET_SSID "*****" //Your wifi name
+#define SECRET_PSW "******" //Your wifi password
+#define SECRET_APIKEY "4cf53cfc2c365ed7e36026acca1dc6c1" //This is my API key, but you can use it.
